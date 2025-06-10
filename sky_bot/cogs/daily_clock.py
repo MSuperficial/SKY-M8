bf131dba2222c268b1a6500041526d9ace06845e
Wang Dezhao <252829652@qq.com>
2025-06-10 11:32:56 +0800

    enhance customize daily_clock events through config

--- sky_bot/cogs/daily_clock.py
+++ sky_bot/cogs/daily_clock.py
@@ -1,4 +1,5 @@
 import asyncio
+from contextlib import suppress
 from datetime import datetime, timedelta
 from typing import Any
 
@@ -6,7 +7,12 @@ from discord.ext import commands
 from discord.utils import format_dt as timestamp
 
 from ..sky_bot import SkyBot
-from ..sky_event.daily import DailyEvent, daily_event_datas, get_daily_event_time
+from ..sky_event.daily import (
+    DailyEventData,
+    fetch_event_data,
+    fetch_events,
+    get_daily_event_time,
+)
 from ..sky_event.shard import get_shard_info
 from ..utils import sky_time_now
 from .base.live_update import LiveUpdateCog
@@ -24,11 +30,10 @@ class DailyClock(
     def __init__(self, bot: SkyBot):
         super().__init__(bot)
 
-    def get_daily_event_msg(self, when: datetime, daily_id: DailyEvent):
-        name = daily_event_datas[daily_id].name
-        current_end, next_begin = get_daily_event_time(when, daily_id)
+    def get_daily_event_msg(self, when: datetime, event_data: DailyEventData):
+        current_end, next_begin = get_daily_event_time(when, event_data)
         # 事件名称
-        msg = f"### {name}\n"
+        msg = f"### {event_data.name}\n"
         # 当前事件结束时间
         if current_end is not None:
             msg += f"-# 🔹 Current ends {timestamp(current_end, 'R')}.\n"
@@ -36,16 +41,18 @@ class DailyClock(
         msg += f"-# 🔸 Next at {timestamp(next_begin, 't')}, {timestamp(next_begin, 'R')}."  # fmt: skip
         return msg
 
-    def get_all_daily_event_msg(self, when: datetime, header=True, footer=True):
-        dailies = list(DailyEvent)
+    async def get_all_daily_event_msg(self, when: datetime, header=True, footer=True):
+        events = await fetch_events()
         shard_info = get_shard_info(when)
         # 如果今天Peaks Shard不提供烛火，则无需显示其信息
         if not (shard_info.has_shard and shard_info.extra_shard):
-            dailies.remove(DailyEvent.PEAKS_SHARD)  # 移除该事件
-        msgs = [self.get_daily_event_msg(when, e) for e in dailies]
+            with suppress(ValueError):
+                events.remove("peaks_shard")  # 移除该事件
+        data = await fetch_event_data()
+        msgs = [self.get_daily_event_msg(when, data[e]) for e in events]
         dailies_msg = "\n".join(msgs)
         if header:
-            dailies_msg = "# Sky Daily Clock\n" + dailies_msg
+            dailies_msg = "# Sky Clock\n" + dailies_msg
         if footer:
             dailies_msg += "\n\n-# *See [Sky Clock](<https://sky-clock.netlify.app>) by [Chris Stead](<https://github.com/cmstead>) for more.*"  # fmt: skip
         return dailies_msg
@@ -54,12 +61,12 @@ class DailyClock(
     async def clock(self, ctx: commands.Context, offset: int = 0):
         now = sky_time_now()
         date = now + timedelta(days=offset)
-        msg = self.get_all_daily_event_msg(date)
+        msg = await self.get_all_daily_event_msg(date)
         await ctx.send(msg)
 
     async def get_live_message_data(self, **kwargs) -> dict[str, Any]:
         now = sky_time_now()
-        content = self.get_all_daily_event_msg(now)
+        content = await self.get_all_daily_event_msg(now)
         return {"content": content}
 
     def check_need_update(self, data: dict[str, Any]):
--- sky_bot/sky_event/daily.py
+++ sky_bot/sky_event/daily.py
@@ -1,95 +1,119 @@
+import json
 from datetime import datetime, timedelta
-from enum import Enum
 from typing import NamedTuple
 
+from ..remote_config import remote_config
+
 __all__ = (
-    "DailyEvent",
-    "daily_event_datas",
+    "DailyEventData",
+    "fetch_events",
+    "fetch_event_data",
     "get_daily_event_time",
 )
 
 
-class DailyEvent(Enum):
-    GEYSER = 10
-    PEAKS_SHARD = 20
-    GRANDMA = 30
-    TURTLE = 40
-    DAILY_RESET = 50
+_EVENTS_KEY = "dailyClock.events"
+
+_default_events = [
+    "geyser",
+    "peaks_shard",
+    "aurora",
+    "grandma",
+    "turtle",
+    "daily_reset",
+]
 
 
 class DailyEventData(NamedTuple):
-    id: DailyEvent
+    id: str
     name: str
     offset: int
-    period: int
     duration: int
+    period: int
 
 
-daily_event_ids = {
-    "geyser": DailyEvent.GEYSER,
-    "peaksshard": DailyEvent.PEAKS_SHARD,
-    "granny": DailyEvent.GRANDMA,
-    "grandma": DailyEvent.GRANDMA,
-    "turtle": DailyEvent.TURTLE,
-    "daily": DailyEvent.DAILY_RESET,
-}
-
-daily_event_datas = {
-    DailyEvent.GEYSER: DailyEventData(
-        id=DailyEvent.GEYSER,
+_daily_event_data = {
+    "geyser": DailyEventData(
+        id="geyser",
         name="⛲ Geyser",
         offset=5,
-        period=120,
         duration=10,
+        period=120,
     ),
-    DailyEvent.PEAKS_SHARD: DailyEventData(
-        id=DailyEvent.PEAKS_SHARD,
+    "peaks_shard": DailyEventData(
+        id="peaks_shard",
         name="🏔️ Peaks Shard",
         offset=8,
-        period=30,
         duration=22,
+        period=30,
     ),
-    DailyEvent.GRANDMA: DailyEventData(
-        id=DailyEvent.GRANDMA,
+    "aurora": DailyEventData(
+        id="aurora",
+        name="🎶 Aurora Concert",
+        offset=10,
+        duration=48,
+        period=240,
+    ),
+    "grandma": DailyEventData(
+        id="grandma",
         name="🍞 Grandma",
         offset=35,
-        period=120,
         duration=10,
+        period=120,
     ),
-    DailyEvent.TURTLE: DailyEventData(
-        id=DailyEvent.TURTLE,
+    "turtle": DailyEventData(
+        id="turtle",
         name="🐢 Turtle",
         offset=50,
-        period=120,
         duration=10,
+        period=120,
     ),
-    DailyEvent.DAILY_RESET: DailyEventData(
-        id=DailyEvent.DAILY_RESET,
+    "daily_reset": DailyEventData(
+        id="daily_reset",
         name="⏱️ Daily reset",
         offset=0,
-        period=24 * 60,
         duration=0,
+        period=24 * 60,
     ),
 }
 
 
-def get_daily_event_time(
-    now: datetime, daily: DailyEvent
-) -> tuple[datetime | None, datetime]:
-    daily_data = daily_event_datas[daily]
+async def fetch_events():
+    events: list[str] = _default_events
+    value = await remote_config.get_field(_EVENTS_KEY, "displayed_events")
+    if value:
+        events = json.loads(value)
+    return events
+
+
+async def fetch_event_data():
+    data = _daily_event_data.copy()
+    overrides: dict[str, dict] = {}
+    value = await remote_config.get_field(_EVENTS_KEY, "event_data_overrides")
+    if value:
+        overrides = json.loads(value)
+    for k, v in overrides.items():
+        if k in data:
+            data[k] = data[k]._replace(**v)
+        else:
+            data[k] = DailyEventData(**v)
+    return data
+
+
+def get_daily_event_time(now: datetime, event_data: DailyEventData):
     # 计算自今天开始经过的分钟数
     now_time = now.replace(second=0, microsecond=0)
     minutes_passed = now_time.hour * 60 + now_time.minute
     # 计算自上次事件开始经过的分钟数
-    minutes_from_last = (minutes_passed - daily_data.offset) % daily_data.period
+    minutes_from_last = (minutes_passed - event_data.offset) % event_data.period
     # 如果当前事件正在进行，计算当前时间结束的时间
     current_end_time = None
-    if minutes_from_last < daily_data.duration:
+    if minutes_from_last < event_data.duration:
         current_end_time = now_time + timedelta(
-            minutes=daily_data.duration - minutes_from_last
+            minutes=event_data.duration - minutes_from_last
         )
     # 计算下次事件开始的时间
     next_begin_time = now_time + timedelta(
-        minutes=daily_data.period - minutes_from_last
+        minutes=event_data.period - minutes_from_last
     )
     return current_end_time, next_begin_time
